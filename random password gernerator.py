e12809de95cbc83c982f104ea824cbef96b96be2
Sagun21 <129666761+Sagun21@users.noreply.github.com>
2023-04-30 00:00:58 +0530

    Add files via upload

---
+++ random password gernerator.py
@@ -0,0 +1,30 @@
+import random
+
+
+def generatePassword(n):
+    
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*()"
+
+    """
+    The random.sample() method returns a list, so we need to convert it into a string before returning it.
+    """
+
+    chosenLetter = random.sample(characters, n)
+
+    
+    password = "".join(chosenLetter)
+
+    return password
+
+
+if __name__ == "__main__":
+    while True:
+        usersChoice = input(
+            "Please enter 'yes' if you want to generate a password else type 'no' to exit: ")
+        if usersChoice == 'no':
+            break
+        else:
+            n = int(input("Enter the length of the password: "))
+            password = generatePassword(n)
+            print("A randomly selected password is:", password)
+
